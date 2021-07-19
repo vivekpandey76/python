b50d54d55e48bc25759896437daf02adeeb3ce05
Vivekpandey76 <Vivekpandey22982@gmail.com>
2021-07-19 18:06:36 +0530

    Initial commit

---
+++ program.py
@@ -0,0 +1,23 @@
+import os
+from datetime import date
+
+today = date.today().strftime("%d-%B-%Y")
+
+def pusher():
+    os.system("git add .")
+    os.system('git commit -m "Committed on : {today} "'.format(today=today))
+    os.system("git push origin main")
+
+def append():
+    file1 = open("myfile.txt", "a")
+    file1.write(f"Modified on : {today} \n")
+    file1.close()
+    
+    file1 = open("myfile.txt", "r")
+    print("Output of Readlines after appending")
+    print(file1.read())
+    print()
+    file1.close()
+
+append()
+pusher()
